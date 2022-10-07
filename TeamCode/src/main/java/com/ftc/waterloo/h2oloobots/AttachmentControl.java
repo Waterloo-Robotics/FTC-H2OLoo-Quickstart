2eed6d1f577c794aab84134fe6a59e7b63786b1e
H2OLooBots <ftcteam7247@gmail.com>
2022-10-06 20:39:31 -0500

    Version 0.1.0

---
+++ TeamCode/src/main/java/com/ftc/waterloo/h2oloobots/AttachmentControl.java
@@ -0,0 +1,29 @@
+package com.ftc.waterloo.h2oloobots;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorColor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class AttachmentControl {
+
+    public static ColorSensor color;
+    public static DistanceSensor distance;
+    public static Servo servo;
+
+    public void attachmentInit(HardwareMap hardwareMap) {
+
+        color = hardwareMap.colorSensor.get("color");
+        distance = (DistanceSensor) hardwareMap.get(DistanceSensor.class, "distance");
+        servo = hardwareMap.servo.get("servo");
+
+    }
+
+}
